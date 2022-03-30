1d546af2ffb19445cf41d0f22c112b48cdee01d9
Ruben Jimenez <rdja06@gmail.com>
2022-03-30 11:21:28 -0400

    first commit

---
+++ calculator.js
@@ -0,0 +1,23 @@
+const express = require("express")
+const bodyParser = require("body-parser")
+
+const app = express();
+app.use(bodyParser.urlencoded({extended:true}))
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + "/bmicalculator.html")
+ })
+
+ app.post("/", function(req, res){
+
+    var weight = parseFloat(req.body.weight);
+    var height = parseFloat(req.body.height);
+
+    var bmi = weight / (height * height);
+
+    res.send("Your BMI is " + bmi);
+})
+
+app.listen (3000, function() {
+    console.log("Working")
+})
